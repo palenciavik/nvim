97f45a5e9e018bf751afddd32325440f095e8c44
palenciaVik <palenciavik@gmail.com>
2024-12-30 19:40:19 -0800

    nerd font

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
