4af80a808f82c5485b19b1ffdf9c4191164f930e
palenciavik <palenciavik@gmail.com>
2025-02-22 00:36:34 -0800

    trying out avante

--- init.lua
+++ init.lua
@@ -99,6 +99,12 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagn
 vim.keymap.set("n", "<leader>E", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+-- Resizing keymaps
+vim.keymap.set("n", "<C-w><Up>", ":resize +2<CR>", { silent = true })
+vim.keymap.set("n", "<C-w><Down>", ":resize -2<CR>", { silent = true })
+vim.keymap.set("n", "<C-w><Left>", ":vertical resize -2<CR>", { silent = true })
+vim.keymap.set("n", "<C-w><Right>", ":vertical resize +2<CR>", { silent = true })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -196,6 +202,73 @@ require("lazy").setup({
 		opts = {},
 	},
 	{ "ThePrimeagen/vim-be-good", event = "VimEnter" },
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+		opts = {
+			-- General opts for avante.nvim
+			provider = "gemini-2.0-flash-thinking",
+			gemini = {
+				-- @see https://ai.google.dev/gemini-api/docs/models/gemini
+				model = "gemini-2.0-pro-exp-02-05",
+				-- model = "gemini-1.5-flash",
+				temperature = 0.5,
+				max_tokens = 8192,
+			},
+			vendors = {
+				["gemini-2.0-flash-thinking"] = {
+					__inherited_from = "gemini",
+					model = "gemini-2.0-flash-thinking-exp-01-21",
+					temperature = 0.5,
+					max_tokens = 8192,
+					timeout = 30000,
+				},
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
